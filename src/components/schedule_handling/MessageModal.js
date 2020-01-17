faa87e0dc8c34f65f1e7fc011200d8b8400769f9
Maria Pascu <mariapascu0498@gmail.com>
2020-01-17 18:31:29 +0200

    Show changelist

--- src/components/schedule_handling/MessageModal.js
+++ src/components/schedule_handling/MessageModal.js
@@ -4,10 +4,13 @@ import {DialogActions, DialogContent} from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Button from "@material-ui/core/Button";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
 
 class MessageModal extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             newClass: this.props.courseClass,
             student: this.props.student,
@@ -17,7 +20,7 @@ class MessageModal extends React.Component {
 
     exitMessageModal = () => {
         this.setState({showModal: false});
-    }
+    };
 
     render() {
         console.log("MARIAAAA");
@@ -25,25 +28,35 @@ class MessageModal extends React.Component {
             <DialogTitle>
                 <div>Write a message for your teacher!</div>
             </DialogTitle>
+            <form onSubmit={this.exitMessageModal}>
             <DialogContent>
-            <TextField fullWidth="true"
-                id="standard-multiline-static"
-                label="Your message"
-                multiline
-                rows="4"
-                defaultValue=""
-            />
+                <TextField fullWidth="true"
+                           id="standard-multiline-static"
+                           label="Your message"
+                           multiline
+                           rows="4"
+                           defaultValue=""
+                />
+                <RadioGroup defaultValue="one-time" className="radioGroup" aria-label="type" name="type" required row>
+                    <FormControlLabel value="one-time" control={<Radio color="primary"/>}
+                                      label="One time change"/>
+                    <FormControlLabel control={<Radio color="primary"/>}
+                                      value="permanent"
+                                      label="Permanent change"/>
+                </RadioGroup>
             </DialogContent>
             <DialogActions>
                 <Button onClick={this.exitMessageModal}>
                     Cancel
                 </Button>
-                <Button onClick={this.exitMessageModal}>
+                <Button type="submit">
                     Send
                 </Button>
             </DialogActions>
-        </Dialog>);
+            </form>
+        </Dialog>
+    );
     }
-}
+    }
 
-export default MessageModal;
+    export default MessageModal;
